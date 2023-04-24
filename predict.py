acaf3f7dfb7ddd623311d14ef6fac92458676648
Termi7 <90416021+Termi7@users.noreply.github.com>
2023-04-24 14:18:50 -0400

    cleanup

--- predict.py
+++ predict.py
@@ -23,13 +23,13 @@ def preprocess_image(img, model_path='my_model.h5', target_size=(128, 128)):
 
     # Get the index of the class with the highest probability
     predicted_class_index = np.argmax(prediction)
-    print(predicted_class_index)
+    # print(predicted_class_index)
 
     # Get the name of the predicted class
     data_dir = os.path.join(os.getcwd(), 'data')
     class_names = sorted(os.listdir(data_dir))
     class_names = [class_name for class_name in class_names if not class_name.startswith('.')]  # add this line
-    print(class_names)
+    # print(class_names)
     predicted_class_name = class_names[predicted_class_index]
 
     return predicted_class_name
